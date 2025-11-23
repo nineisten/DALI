928d317c613bed0050ba47635c2080cd15e60e67
nineisten <nineisx@icloud.com>
2025-11-22 21:04:19 -0500

    chore:removed debris

--- api/views/mods/main_header.ts
+++ api/views/mods/main_header.ts
@@ -6,11 +6,10 @@ const MainHeader = (user:true) =>/*html*/`
     <ul>
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
-        <li><a href="#ICO ">ICO</a></li>
         <li><a href="#contact">Contact</a></li>
     </ul>
     <button>
-        wallet connect
+        connect
     </button>
 </header>
 `
